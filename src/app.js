2104088941acdbdd38762192177b4b60d1b02c81
arielrahmane <arielrahmane@gmail.com>
2019-05-09 16:44:51 -0300

    Structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 8081)
