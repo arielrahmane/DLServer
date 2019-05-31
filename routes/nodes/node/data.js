f2cdd92df941750d2e1b49efca9f40d7841ab958
arielrahmane <arielrahmane@gmail.com>
2019-05-31 18:23:40 -0300

    Nodes data endpoint created

--- routes/nodes/node/data.js
+++ routes/nodes/node/data.js
@@ -2,8 +2,25 @@
 
 const {NodesData} = require("../../../src/database");
 
+function findLatestEntry(node, res) {
+	NodesData.findOne({
+		where: {
+			nodeID: node
+		},
+		order: [['createdAt', 'DESC']]
+	})
+	.then(nodeData => {
+		console.log("THIS IS THE NODE DATA: ", JSON.stringify(nodeData));
+		res.send(JSON.stringify(nodeData));
+	})
+	.catch(err => {
+		res.status(400).send(err);
+	})
+}
+
 module.exports = app => {
 	app.get('/nodes/:node', (req, res) => {
-		console.log("Entered to /nodes/", req.body.node);
+		var node = req.params.node;
+		findLatestEntry(node, res);
 	});
 };
