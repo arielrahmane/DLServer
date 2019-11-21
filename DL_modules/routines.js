ee72d1d11df33da7b7542962eeac1779fffcdf2b
arielrahmane <arielrahmane@gmail.com>
2019-11-20 22:37:48 -0300

    Hourly routine defined in testJob (need to be implemented)

--- DL_modules/routines.js
+++ DL_modules/routines.js
@@ -27,13 +27,55 @@ const monthJob = new CronJob('0 0 0 1 * *', function() {
 });
 
 //Test routine
-const testJob = new CronJob('* * * * * *', function() {
-	// dbStorage.getNodesDataSpan(14, "2019-11-12 01:58:00", "2019-11-12 02:20:00")
-	//.then(data => {console.log(data)});
-	var date = moment().format("YYYY-MM-DD");
-	var time = moment().format("HH:mm:ss");
-	console.log(date + " " + time);
+var hour = 23;
+var day = 11;
+const testJob = new CronJob('5 * * * * *', function() {
+	var tempA = []; 
+	var tempB = []; 
+	var tempC = []; 
+	var humidA = []; 
+	var humidB = []; 
+	var humidC = []; 
+	var alcohol = []; 
+
+	//var date1 = moment().subtract(0, "days").format("YYYY-MM-DD");
+	//var time1 = moment().subtract(0, "hours").format("HH:mm:ss");
+	var date1 = "2019-11-" + day.toString();
+	var time1 = hour.toString() + ":00:00";
+	var datetime1 = date1 + " " + time1;
+	//var date2 = moment().subtract(0, "days").format("YYYY-MM-DD");
+	//var time2 = moment().subtract(0, "hours").format("HH:mm:ss");
+	hour++;
+	if (hour > 23) {
+		hour = 00;
+		day++; //Cuidado con pasarse de fechas permitidas
+	};
+	var date2 = "2019-11-" + day.toString();
+	var time2 = hour.toString() + ":00:00";
+	var datetime2 = date2 + " " + time2;
+	dbStorage.getNodesDataSpan(14, datetime1, datetime2)
+	.then(data => {
+		var length = data.length;
+		for (var i = 0; i<length; i++) {
+			tempA.push(data[i].dataValues.tempA); 
+			tempB.push(data[i].dataValues.tempB); 
+			tempC.push(data[i].dataValues.tempC); 
+			humidA.push(data[i].dataValues.humidA); 
+			humidB.push(data[i].dataValues.humidB); 
+			humidC.push(data[i].dataValues.humidC);
+			alcohol.push(data[i].dataValues.alcohol); 
+		}
+		alcoholAvg = getAvg(alcohol);
+		console.log("Promedio de tempA: " + alcoholAvg.toFixed(2));
+	});
+
+	//Output ---> [NodesData: {dataValues}, ]
 });
+
+function getAvg(arr) {
+	var sum = arr.reduce((a,b) => a + b, 0);
+	return sum/arr.length;
+}
 
 module.exports = {
     dailyJob,
