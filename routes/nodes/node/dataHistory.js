f58b4ccce1238daba7eb6c534bd05bdd3f85bb3a
arielrahmane <arielrahmane@gmail.com>
2019-11-22 15:18:01 -0300

    Developing function for request node data in a period of time

--- routes/nodes/node/dataHistory.js
+++ routes/nodes/node/dataHistory.js
@@ -1,6 +1,8 @@
 // This is the routes to get individuals nodes data history
 
-const {NodesData} = require("../../../src/database");
+const {NodesData, NodesHourAv, NodesDailyAv, NodesMonthlyAv} = require("../../../src/database");
+const DBStorage = require("../../../DL_modules/DBstorage");
+const _ = require('lodash');
 
 function findNodeHistory(node, field, res) {
 	NodesData.findAll({
@@ -17,12 +19,67 @@ function findNodeHistory(node, field, res) {
 	.catch(err => {
 		res.status(400).send(err);
 	})
+};
+
+function getNodesDataSet(node, field, params, res) {
+	if (_.isEmpty(params)) {
+		findNodeHistory(node, field, res);
+	} else {
+		var timeSpan = params.timeSpan;
+		var fromDate = params.fromDate;
+		var toDate = params.toDate;
+
+		switch (timeSpan) {
+			case "minutes":
+				DBStorage.getVariableSpan(NodesData, node, field, fromDate, toDate)
+				.then(dataSet => {
+					res.send(dataSet);
+				})
+				.catch(err => {
+					res.status(400).send(err);
+				});
+				break;
+			case "hours":
+				DBStorage.getVariableSpan(NodesHourAv, node, field, fromDate, toDate)
+				.then(dataSet => {
+					res.send(dataSet);
+				})
+				.catch(err => {
+					res.status(400).send(err);
+				});
+				break;
+			case "days":
+				DBStorage.getVariableSpan(NodesDailyAv, node, field, fromDate, toDate)
+				.then(dataSet => {
+					res.send(dataSet);
+				})
+				.catch(err => {
+					res.status(400).send(err);
+				});
+				break;
+			case "days":
+				DBStorage.getVariableSpan(NodesMonthlyAv, node, field, fromDate, toDate)
+				.then(dataSet => {
+					res.send(dataSet);
+				})
+				.catch(err => {
+					res.status(400).send(err);
+				});
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 module.exports = app => {
+	/* 
+		/nodes/:node/history/:field?timeSpan=days&fromDate="2019-11-11 12:00:00"&toDate="2019-11-12 12:00:00"
+		timeSpan = [minutes, hours, days, months] (possible values)
+	*/
 	app.get('/nodes/:node/history/:field', (req, res) => {
 		var node = req.params.node;
 		var field = (req.params.field).toString();
-		findNodeHistory(node, field, res);
+		getNodesDataSet(node, field, req.query, res);
 	});
 };
